5f4805d2200c415807d388d8b2d89af1f9723a5b
thiago <thiagoedusan5.11@outlook.com>
2022-09-21 19:29:23 -0300

    feat: rota resgister criada

---
+++ back-end/src/api/middlewares/validJoiRegister.js
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+
+const createUserDto = Joi.object({
+  name: Joi.string().min(12).required(),
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: false } }).required(),
+  password: Joi.string().min(6).required(),
+  role: Joi.string().required(),
+});
+
+const createUserValid = (req, res, next) => {
+  const { error } = createUserDto.validate(req.body);
+  if (!error) {
+    return next();
+  }
+  const [message] = error.details.map((e) => e.message);
+  return res.status(400).json({ message });
+};
+
+module.exports = createUserValid;
---
+++ back-end/src/api/services/registerService.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const md5 = require('md5');
+const { User } = require('../../database/models');
+const { JWT_SECRET } = process.env;
+
+const register = async ({ name, email, password, role }) => {
+  const foundUser = await User.findOne({ where: { email } });
+  if (foundUser) return { statusCode: 409, result: { message: 'User already registered' } };
+
+  const criptPassword = md5(password);
+  const { id } = await User.create({ name, email, password: criptPassword, role });
+  const config = { expiresIn: '7d', algorithm: 'HS256' };
+  const payload = { email, id };
+  const token = jwt.sign(payload, JWT_SECRET, config);
+
+  return { statusCode: 201, result: { token } };
+};
+
+module.exports = {
+  register,
+};
