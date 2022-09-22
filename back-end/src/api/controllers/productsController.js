903dbe38b63912a22cee39fc1a7e3081aec12f7b
thiago <thiagoedusan5.11@outlook.com>
2022-09-22 18:58:54 -0300

    feat: foi feita a rota "products:id"

--- back-end/src/api/controllers/productsController.js
+++ back-end/src/api/controllers/productsController.js
@@ -6,6 +6,13 @@ const getAllProducts = async (req, res) => {
   return res.status(statusCode).json(result);
 };
 
+const getProductById = async (req, res) => {
+  const { statusCode, result } = await productsService.getProductById(req.params.id);
+
+  return res.status(statusCode).json(result);
+}
+
 module.exports = {
   getAllProducts,
+  getProductById,
 };
--- back-end/src/api/services/productsService.js
+++ back-end/src/api/services/productsService.js
@@ -6,6 +6,14 @@ const getAllProducts = async () => {
   return { statusCode: 200, result: response };
 };
 
+const getProductById = async (id) => {
+  const response = await Product.findOne({ where: { id } });
+  if (!response) return { statusCode: 404, result: { message: 'Product does not exist' }  };
+
+  return { statusCode: 200, result: response }
+}
+
 module.exports = {
   getAllProducts,
+  getProductById,
 };
