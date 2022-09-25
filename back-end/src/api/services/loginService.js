dbd3f25190c23149b3e90f574be1d945c5de12ab
Igor <igormaciel82@gmail.com>
2022-09-25 16:36:39 -0300

    refactor: Utiliza o módulo fs para gerar a chave secreta do token

--- back-end/src/api/services/loginService.js
+++ back-end/src/api/services/loginService.js
@@ -1,10 +1,9 @@
 const jwt = require('jsonwebtoken');
-
 const md5 = require('md5'); 
-
+const fs = require('fs').promises;
 const { User } = require('../../database/models');
 
-const { JWT_SECRET } = process.env;
+const JWT_SECRET = async () => fs.readFile('./jwt.evaluation.key', 'utf-8');
 
 const login = async ({ email, password }) => {
   const findUser = await User.findOne({ where: { email } });
@@ -17,9 +16,11 @@ const login = async ({ email, password }) => {
     return { statusCode: 404, result: { message: 'Incorrect email or password' } };
   }
 
+  const secret = await JWT_SECRET();
   const { name, email: userEmail, role } = findUser;
   const payload = { email, id: findUser.id };
-  const token = jwt.sign(payload, JWT_SECRET, config);
+  const token = jwt.sign(payload, secret, config);
+  
   return { statusCode: 200, result: { name, email: userEmail, role, token } };
 };
 
--- back-end/src/api/services/registerService.js
+++ back-end/src/api/services/registerService.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken');
 const md5 = require('md5');
+const fs = require('fs').promises;
 const { User } = require('../../database/models');
 
-const { JWT_SECRET } = process.env;
+const JWT_SECRET = async () => fs.readFile('./jwt.evaluation.key', 'utf-8');
 
 const register = async ({ name, email, password, role }) => {
   const foundUser = await User.findOne({ where: { email } });
@@ -14,7 +15,8 @@ const register = async ({ name, email, password, role }) => {
   const { id } = await User.create({ name, email, password: criptPassword, role: defaultRole });
   const config = { expiresIn: '7d', algorithm: 'HS256' };
   const payload = { email, id };
-  const token = jwt.sign(payload, JWT_SECRET, config);
+  const secret = await JWT_SECRET();
+  const token = jwt.sign(payload, secret, config);
 
   return { statusCode: 201, result: { name, email, role, token } };
 };
