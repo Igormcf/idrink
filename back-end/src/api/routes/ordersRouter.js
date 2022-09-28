88dec4a59a0b1a599cab8a1c1ba93ef47cdec037
Igor <igormaciel82@gmail.com>
2022-09-28 14:55:59 -0300

    refactor: Altera endpoint para retornar todas as vendas de determinado vendedor

--- back-end/src/api/routes/ordersRouter.js
+++ back-end/src/api/routes/ordersRouter.js
@@ -4,7 +4,7 @@ const ordersController = require('../controllers/ordersController');
 
 const router = express.Router();
 
-router.get('/seller/:id', validJWT, ordersController.getAllSalesBySeller);
+router.get('/seller', validJWT, ordersController.getAllSalesBySeller);
 router.patch('/:id', validJWT, ordersController.updateSalesBySeller);
 
 module.exports = router;
