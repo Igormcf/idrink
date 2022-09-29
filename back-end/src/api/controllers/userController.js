d61e3cee7bc879796a63bf67514efe4d3e65952a
jvictorcabral <joaovictorcgjvcg@gmail.com>
2022-09-29 18:16:43 -0400

    feat: implentando logica do botão de deletar

--- back-end/src/api/controllers/userController.js
+++ back-end/src/api/controllers/userController.js
@@ -1,6 +1,6 @@
 const service = require('../services/userService');
 
-const getAllUsers = async (req, res) => {
+const getAllUsers = async (_req, res) => {
   const { statusCode, result } = await service.getAllUsers();
 
   return res.status(statusCode).json(result);
