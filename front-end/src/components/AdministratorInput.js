8761a72edb0a2f4e46ed0b38c8534c3da35e0075
Igor <igormaciel82@gmail.com>
2022-10-06 17:30:40 -0300

    feat: Implementa layout da tela do administrador

--- front-end/src/components/AdministratorInput.js
+++ front-end/src/components/AdministratorInput.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 // import { useHistory } from 'react-router-dom';
 import Button from './Button';
+import '../css/Adm.css';
 
 export default function RegisterInput() {
   const [userName, setUserName] = useState('');
@@ -30,6 +31,13 @@ export default function RegisterInput() {
     && role !== ''
   );
 
+  const cleanForm = () => {
+    setUserName('');
+    setUserEmail('');
+    setUserPassword('');
+    setRole('');
+  };
+
   const buttonRegister = async () => {
     const url = 'http://localhost:3001/register'; // 'http://localhost:3001/admin/manage';
     const data = JSON.parse(localStorage.getItem('user'));
@@ -57,6 +65,7 @@ export default function RegisterInput() {
     }
     await fetch(url, requestOptions);
     setChangeStatus(!changeStatus);
+    cleanForm();
   };
 
   const removeUserById = async (id) => {
@@ -94,63 +103,70 @@ export default function RegisterInput() {
   }, [changeStatus]);
 
   return (
-    <div>
-      <label htmlFor="name">
-        Nome
-        <input
-          data-testid="admin_manage__input-name"
-          placeholder="Nome"
-          type="text"
-          id="name"
-          onChange={ ({ target }) => setUserName(target.value) }
-        />
-      </label>
+    <main className="main-details">
+      <h3>Cadastrar novo usuário</h3>
+      <form className="form-admin">
+        <label htmlFor="name">
+          Nome
+          <input
+            data-testid="admin_manage__input-name"
+            placeholder="Nome"
+            type="text"
+            id="name"
+            value={ userName }
+            onChange={ ({ target }) => setUserName(target.value) }
+          />
+        </label>
 
-      <label htmlFor="email">
-        Login
-        <input
-          data-testid="admin_manage__input-email"
-          placeholder="Ex: your@email.com"
-          type="email"
-          id="email"
-          onChange={ ({ target }) => setUserEmail(target.value) }
-        />
-      </label>
+        <label htmlFor="email">
+          Login
+          <input
+            data-testid="admin_manage__input-email"
+            placeholder="Ex: your@email.com"
+            type="email"
+            id="email"
+            value={ userEmail }
+            onChange={ ({ target }) => setUserEmail(target.value) }
+          />
+        </label>
 
-      <label htmlFor="password">
-        Senha
-        <input
-          data-testid="admin_manage__input-password"
-          placeholder="******"
-          type="password"
-          id="password"
-          onChange={ ({ target }) => setUserPassword(target.value) }
-        />
-      </label>
+        <label htmlFor="password">
+          Senha
+          <input
+            data-testid="admin_manage__input-password"
+            placeholder="******"
+            type="password"
+            id="password"
+            value={ userPassword }
+            onChange={ ({ target }) => setUserPassword(target.value) }
+          />
+        </label>
 
-      <label htmlFor="user-type">
-        Tipo
-        <select
-          id="user-type"
-          data-testid="admin_manage__select-role"
-          defaultValue=""
-          onChange={ ({ target }) => setRole(target.value) }
+        <label htmlFor="user-type">
+          Tipo
+          <select
+            id="user-type"
+            data-testid="admin_manage__select-role"
+            defaultValue=""
+            value={ role }
+            onChange={ ({ target }) => setRole(target.value) }
+          >
+            <option value="" disabled hidden>Escolha o tipo</option>
+            <option value="customer">Consumidor</option>
+            <option value="seller">Vendedor</option>
+            <option value="administrator">Administrador</option>
+          </select>
+        </label>
+
+        <Button
+          dataTestid="admin_manage__button-register"
+          disabled={ isNotFormValid() }
+          onClick={ () => { buttonRegister(); } }
+          className="btn red_button"
         >
-          <option value="" disabled hidden>Escolha o tipo</option>
-          <option value="customer">Consumidor</option>
-          <option value="seller">Vendedor</option>
-          <option value="administrator">Administrador</option>
-        </select>
-      </label>
-
-      <Button
-        dataTestid="admin_manage__button-register"
-        disabled={ isNotFormValid() }
-        onClick={ () => { buttonRegister(); } }
-      >
-        Cadastrar
-      </Button>
-
+          Cadastrar
+        </Button>
+      </form>
       {
         userExists ? (
           <p data-testid="admin_manage__element-invalid-register">
@@ -159,8 +175,8 @@ export default function RegisterInput() {
         )
           : <p className="error-message invisible">text</p>
       }
-
-      <table>
+      <h3>Lista de usuários</h3>
+      <table className="table-adm">
         <thead>
           <tr>
             <th>Item</th>
@@ -173,7 +189,7 @@ export default function RegisterInput() {
         <tbody>
           {
             users.map(({ id, name, email, role: userRole }, index) => (
-              <tr key={ `adminTable_${index}` }>
+              <tr key={ `adminTable_${index}` } className="tr-table-adm">
                 <td
                   data-testid={
                     `admin_manage__element-user-table-item-number-${index}`
@@ -206,6 +222,7 @@ export default function RegisterInput() {
                   <Button
                     dataTestid={ `admin_manage__element-user-table-remove-${index}` }
                     onClick={ () => removeUserById(id) }
+                    className="btn-table-adm"
                   >
                     Excluir
                   </Button>
@@ -216,6 +233,6 @@ export default function RegisterInput() {
         </tbody>
       </table>
 
-    </div>
+    </main>
   );
 }
